0d3406ad2f88cb105ba16cba5367f02815c396f6
vivektamore <vivektamore91@gmail.com>
2023-01-13 15:09:37 +0530

    add sanity to my frontend

---
+++ sanity backend/dist/static/index-6ea4c5fb-494e2060.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-66ec8894-4c658a6a.js";import"./desk-eb7b3548-97128869.js";import"./index-52809b41.js";export{m as DocumentPaneProvider,a as default};
